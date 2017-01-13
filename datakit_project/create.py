45f80ca2e9ed93589f3ea71b7c40480f9f24a680
Serdar Tumgoren <zstumgoren@gmail.com>
2017-01-13 10:19:22 -0800

    Adds test for project creation when no default repo exists and some stub code for next test

--- datakit_project/create.py
+++ datakit_project/create.py
@@ -1,6 +1,6 @@
 import logging
-import os
 
+from cookiecutter.main import cookiecutter
 from cliff.command import Command
 
 
@@ -11,9 +11,49 @@ class Create(Command):
 
     def get_parser(self, prog_name):
         parser = super(Create, self).get_parser(prog_name)
-        parser.add_argument('name', help="Name of project to create")
+        # NOTE: optional by default
+        parser.add_argument(
+            'template',
+            default='',
+            help="Local path/Github URL for a Cookiecutter template"
+        )
+        # TODO: parser.add_argument(
+        #    '--make-default',
+        # help="Make the template the default for new projects"
+        # )
         return parser
 
     def take_action(self, parsed_args):
-        self.log.info("CREATE PROJECT: {}".format(parsed_args.name))
+        # Create project skeleton
+        template = self._get_template(parsed_args)
+        if template:
+            self.log.info("Creating project: {}".format(parsed_args.template))
+            cookiecutter(
+                template,
+                overwrite_if_exists=True,
+            )
+        else:
+            msg = 'No project templates have been installed. You must ' +\
+                'specify the local path or URL to a Cookiecutter project repo'
+            msg2 = "foobar"
+            self.log.info(msg)
+            print(msg)
+        # TODO: if parserd_args.make_default, update ~/.datakit/plugins/datakit_project.json
+        # TODO: if datakit-vcs plugin, call it's bootstrap method
+        # TODO: if project_management (i.e. Gitlab), call it's bootstrap method
         return parsed_args.name
+
+    # PRIVATE
+
+    def _get_template(self, parsed_args):
+        if parsed_args.template != '':
+            return parsed_args.template
+        else:
+            return self._default_template
+
+    @property
+    def _default_template(self):
+        # TODO: Get cookiecutter home directory using Cookiecutter API and check
+        # the plugin's config file .../.datakit/plugins/datakit_project.json for the 
+        # default_template setting
+        pass
--- tests/test_create.py
+++ tests/test_create.py
@@ -1,14 +1,40 @@
-import os
 from unittest import mock
+
+import cookiecutter.config as cc_config
 
 from datakit_project import Create
 
-def test_project_creation(tmpdir):
-    parsed_args = mock.Mock()
-    parsed_args.name = "test project"
+
+def test_warning_when_no_default_repo(caplog, monkeypatch, tmpdir):
+    monkeypatch.setitem(
+        cc_config.DEFAULT_CONFIG,
+        'cookiecutters_dir',
+        tmpdir
+    )
     cmd = Create(None, None, cmd_name='project:create')
-    expected = ['analysis', 'config', 'data', 'etl']
-    # Change working directory to tmp dir
-    tmpdir.chdir()
+    parsed_args = mock.Mock()
+    parsed_args.template = ''
     cmd.run(parsed_args)
-    assert tmpdir.listdir() == expected
+    msg = 'No project templates have been installed'
+    assert msg in caplog.text
+
+
+"""
+def test_create_with_no_previously_loaded_template(monkeypatch, tmpdir):
+    parsed_args = mock.Mock()
+    parsed_args.template = "fake-repo"
+    monkeypatch.setitem(
+        cookiecutter.config.DEFAULT_CONFIG,
+        'cookiecutters_dir',
+        tmpdir
+    )
+    cmd = Create(None, None, cmd_name='project:create')
+    # Change working directory to tmp dir
+    # tmpdir.chdir()
+    # parsed_args.template = 'tests/fake-repo/'
+    # TODO: should raise MissingTemplateError
+    with pytest.raises(cookiecutter.exceptions.RepositoryNotFound):
+        cmd.run(parsed_args)
+        assert tmpdir.listdir() == expected
+        cookiecutter_dir = tmpdir.join('.cookiecutter')
+"""
