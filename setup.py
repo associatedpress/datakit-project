858fb9aca2328300a5abc2cadffe4aefc07f82e2
Serdar Tumgoren <zstumgoren@gmail.com>
2019-07-12 11:10:40 -0700

    Patch version 0.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ A project-skeleton generator for use with the `datakit <https://pypi.python.org/
 from setuptools import setup, find_packages
 
 PROJECT = 'datakit-project'
-VERSION = '0.3.0'
+VERSION = '0.3.1'
 
 setup(
     name=PROJECT,
